6b031ec4e9bc792f53ce8effc3549455ce87c203
Leyla <lkursat@stanford.edu>
2019-05-02 17:45:33 -0700

    added audio and comprehension checks

--- js/index.js
+++ js/index.js
@@ -8,31 +8,74 @@ function make_slides(f) {
     }
   });
 
+   slides.check = slide({
+    name : "check",
+    start: function() {
+      $('.err').hide();
+      $('.question').hide();
+          document.getElementById("audio_check").play();
+          setTimeout(function(){
+            $('.question').show();
+          },1500);
+    },
+    button : function() {
+      this.radio  = $("input[name='number']:checked").val();
+      if (this.radio == "Four") {
+        this.log_responses();
+        exp.go();
+      } 
+      else{
+        $('.err').show();
+        this.log_responses();
+      }
+    },
+    log_responses : function() {
+      exp.data_trials.push({
+          "slide_number": exp.phase,
+          "slide_type" : "audio_check",
+          //"stim" : "",
+          "image" : "",
+          "audio" : "",
+          "response" : [0,this.radio]
+      });
+    }
+  });
+
   slides.instructions = slide({
     name : "instructions",
     start: function(){
+      $('.tut_key_instructions').hide();
       $('.tut_initial_gumball').hide();
       $('.tut_final_gumball').hide();
       $('.tut_final2_gumball').hide();
       $('.tut_agreed').hide();
       $('.tut_disagreed').hide();
       $('.tut_warning').hide();
+      $('.comprehension').hide();
+      $('.err_wrong').hide();
 
       document.onkeydown = checkKey;
-      
       function checkKey(e) {
         e = e || window.event;
-
         if (($('.tut_instructions').is(":visible")) && (e.keyCode == 32)) {
+          e = 0;
           $('.tut_instructions').hide();
+          $('.tut_key_instructions').show();
+        }
+        if (($('.tut_key_instructions').is(":visible")) && (e.keyCode == 32)) {
+          $('.tut_key_instructions').hide();
           $('.tut_initial_gumball').show();
           setTimeout(function(){
-          $('.tut_initial_gumball').hide();
-          document.getElementById("tut_kaching").play();
-          $('.tut_final_gumball').show();  
-          setTimeout(function(){
-            document.getElementById("tut_stim").play(); 
-          },1000)
+            $('.tut_initial_gumball').hide();
+            $('.tut_final_gumball').show();
+            document.getElementById("tut_kaching").play();
+            $('.tut_pressyes').hide();
+            setTimeout(function(){
+              document.getElementById("tut_stim").play();
+              setTimeout(function(){
+                $('.tut_pressyes').show();
+              },1000)
+            },1000)
           },2000)
         }
         if (($('.tut_final_gumball').is(":visible")) && (e.keyCode == 70)) {
@@ -41,17 +84,23 @@ function make_slides(f) {
         if (($('.tut_final_gumball').is(":visible")) && (e.keyCode == 74)) {
           $('.tut_final_gumball').hide();
           $('.tut_agreed').show();
+          document.getElementById("tut_stim").pause();
+          document.getElementById("tut_stim").currentTime = 0;
         }
         if (($('.tut_agreed').is(":visible")) && (e.keyCode == 32)) {
           $('.tut_agreed').hide();
           $('.tut_initial_gumball').show();
           setTimeout(function(){
-          $('.tut_initial_gumball').hide();
-          document.getElementById("tut_kaching").play();
-          $('.tut_final2_gumball').show();  
-          setTimeout(function(){
-            document.getElementById("tut_stim2").play(); 
-          },1000)
+            $('.tut_initial_gumball').hide();
+            $('.tut_final2_gumball').show();
+            document.getElementById("tut_kaching").play();
+            $('.tut_pressno').hide();
+            setTimeout(function(){
+              document.getElementById("tut_stim2").play();
+              setTimeout(function(){
+                $('.tut_pressno').show();
+              },1000)
+            },1000)
           },2000)
         }
         if (($('.tut_final2_gumball').is(":visible")) && (e.keyCode == 74)) {
@@ -60,31 +109,66 @@ function make_slides(f) {
         if (($('.tut_final2_gumball').is(":visible")) && (e.keyCode == 70)) {
           $('.tut_final2_gumball').hide();
           $('.tut_disagreed').show();
+          document.getElementById("tut_stim2").pause();
+          document.getElementById("tut_stim2").currentTime = 0;
         }
-        if (($('.tut_disagreed').is(":visible")) && (e.keyCode == 70)) {
+        if (($('.tut_disagreed').is(":visible")) && (e.keyCode == 32)) {
+          e = 0;
           $('.tut_disagreed').hide();
           $('.tut_warning').show();
         }
-        if (($('.tut_warning').is(":visible")) && (e.keyCode == 70)) {
-          exp.go();
+        if (($('.tut_warning').is(":visible")) && (e.keyCode == 32)) {
+          e = 0;
+          $('.tut_warning').hide();
+          $('.comprehension').show();
+          $('.err_empty').hide();
+          $("input[name='fired']:checked").val = undefined;
         }
+        if (($('.comprehension').is(":visible")) && (e.keyCode == 32)) {
+          e = 0;
+          this.radio = $("input[name='fired']:checked").val();
+          if (this.radio == undefined) {
+            $('.err_empty').show();
+          }
+          if(this.radio == "Empty"){
+            //this.log_responses();                                                      // FIX THIS!!!!!!!!!!
+            exp.go();  
+          }
+          if ((this.radio == "Jam")| (this.radio == "Explode")| (this.radio == "Silent")){
+            $('.comprehension').hide();
+            $('.err_wrong').show();
+            //this.log_responses();                                                      // FIX THIS!!!!!!!!!!
+          }
+        }
+        if (($('.err_wrong').is(":visible")) && (e.keyCode == 32)) {
+            $('.err_wrong').hide();
+            $('.tut_instructions').show();
+        }
+        
       }
     },
-  });
 
-  slides.comprehension = slide({
-    name : "comprehension",
-    start: function() {
-      console.log("comprehension questions");
-    },
-    button : function() {
-      this.log_responses();
-      exp.go();
-    },
     log_responses : function() {
-      exp.catch_trials.push({
+      exp.data_trials.push({
           "slide_number": exp.phase,
+          "slide_type" : "comprehension_check",
+          //"stim" : "",
+          "image" : "",
+          "audio" : "",
+          "response" : [0,this.radio]
       });
+    }
+  });
+  
+  slides.before_trial = slide({
+    name : "before_trial",
+    start : function() {
+      document.onkeydown = checkKey;
+      function checkKey(e) {
+        e = e || window.event;
+        if (e.keyCode == 32) 
+          exp.go();
+      }
     }
   });
 
@@ -101,34 +185,29 @@ function make_slides(f) {
       $('.transition').hide();
       $('.initial_gumball').show()
 
-      var initial_image = '<img src="img/initial.jpg" style="height:500px;">';
+      var initial_image = '<img src="img/initial.jpg" style="height:500px" class="center">';
       $(".initial_image").html(initial_image);
 
-      var final_image = '<img src="img/'+stim.image+'.jpg" style="height:500px;">';
+      var final_image = '<img src="img/'+stim.image+'.jpg" style="height:500px" class="center">';
       $(".final_image").html(final_image);
       
-      //var aud = '<source src="audio/'+stim.audio+'" type="audio/wav">';
-      //document.getElementById("stim").innerHTML = aud;
-      //$(".stim").html(aud);
-
       var aud = document.getElementById("stim");
       aud.src = "audio/"+stim.audio;
       aud.load();
       
       console.log("Beginning of trial, TIME: " + (Date.now()-exp.test_start));                       // TESTING
-      console.log(stim.image);                                                                       // TESTING
-      console.log(stim.audio);                                                                       // TESTING
+      //console.log(stim.image);                                                                     // TESTING
+      //console.log(stim.audio);                                                                     // TESTING
   
       setTimeout(function(){
         $('.initial_gumball').hide();
         document.getElementById("kaching").play();
         $('.final_gumball').show();
-        console.log("Gumball image changed, TIME: " + (Date.now()-exp.test_start));                  // TESTING 
-        
+        console.log("Gumball image changed, TIME: " + (Date.now()-exp.test_start));                  // TESTING
         setTimeout(function(){
-          document.getElementById("stim").play(); 
+          aud.play(); 
           console.log("Gumball audio played, TIME: " + (Date.now()-exp.test_start));                 // TESTING
-          console.log(document.getElementById("stim"));
+          //console.log(document.getElementById("stim"));                                            // TESTING
           exp.startTime = Date.now();
           console.log("Timer started, TIME: " + (Date.now()-exp.test_start));                        // TESTING
         },1000)
@@ -138,12 +217,19 @@ function make_slides(f) {
       function checkKey(e) {
         e = e || window.event;
         if (($('.final_gumball').is(":visible")) && (e.keyCode == 70 || e.keyCode == 74)) {
-          console.log("Pressed J or F, Timer ended");                                               // TESTING
           exp.responseTime = Date.now()-exp.startTime;
+          console.log("Timer ended, TIME: " + (Date.now()-exp.test_start));
+          console.log("Response time: "+  exp.responseTime);                                         // TESTING
+          // if(e.keyCode == 74)
+          //   exp.KeyCode = "Yes";
+          // if(e.keyCode == 70)
+          //   exp.KeyCode = "No";
+          //console.log(exp.KeyCode);
           exp.keyCode = e.keyCode;
-          console.log(exp.responseTime);                                                            // TESTING
           $('.final_gumball').hide();
           $('.transition').show();
+          aud.pause();
+          aud.currentTime = 0;
         } 
         if (($('.transition').is(":visible")) && (e.keyCode == 32)) {
           _s.button();
@@ -154,15 +240,14 @@ function make_slides(f) {
       this.log_responses();
       _stream.apply(this);
     },
-
     log_responses : function() {
       exp.data_trials.push({
           "slide_number": exp.phase,
-          "stim" : this.stim.sentence,
+          "slide_type" : "critical_trial",
+        //  "stim" : this.stim.sentence,
           "image" : this.stim.image,
           "audio" : this.stim.audio,
-          "rt" : exp.responseTime,
-          "key" : exp.keyCode,
+          "response" : [exp.responseTime, exp.keyCode]
       });
     }
   });
@@ -181,7 +266,7 @@ function make_slides(f) {
         problems: $("#problems").val(),
         fairprice: $("#fairprice").val()
       };
-      exp.go(); //use exp.go() if and only if there is no "present" data.
+      exp.go();
     }
   });
 
@@ -208,7 +293,7 @@ function init() {
   exp.trials = [];
   exp.catch_trials = [];
 
-  exp.condition = "empty context - relevant condition";
+  exp.condition = "relevant condition(empty context)";
 
 quarter_1 =[
   {sentence: "You got some gumballs.", image: "0", audio: "some.wav"},
@@ -307,7 +392,7 @@ quarter_4 = [
     };
 
   //blocks of the experiment:
-  exp.structure=["i0", "instructions", "comprehension", "trial", 'subj_info', 'thanks'];
+  exp.structure=["i0", "check","instructions", "before_trial", "trial", 'subj_info', 'thanks'];
 
   exp.data_trials = [];
   //make corresponding slides:
