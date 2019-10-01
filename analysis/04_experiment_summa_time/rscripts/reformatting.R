fbbdfffc023634eb008e7e077385abe322dae4b8
Leyla <lkursat@stanford.edu>
2019-10-01 23:16:43 +0300

    time pressure readme

---
+++ analysis/04_experiment_summa_time/rscripts/reformatting.R
@@ -0,0 +1,24 @@
+# Create new csv with two seperate columns for "rating" and "strange sentence
+
+library(tidyverse)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd('../data/')
+
+df <- read.csv("results_merged.csv")
+
+df <- separate(df,response,into=c("rawRT","key"),sep=",")
+
+df$rawRT <- as.character(gsub("\\[","",df$rawRT))
+df$key <- as.character(gsub("\\]","",df$key))
+
+df$key <- as.character(gsub("\\'","",df$key))
+df$key <- as.character(gsub("\\'","",df$key))
+df$key <- as.character(gsub("\\ ","",df$key))
+
+df <- separate(df,speaker_cond,into=c("age_group","qud"),sep="_")
+
+df$age_group <- as.character(gsub("\\ ","",df$age_group))
+
+write.csv(df, file = "results_formatted.csv")
+
+View(df)
